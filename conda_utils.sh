ce7de3720e2194eff8dd44cc46ba1c6c41fcf659
John Shiver <john@johnshiver.org>
2024-03-28 18:59:24 -0400

    fix init conda

--- conda_utils.sh
+++ conda_utils.sh
@@ -5,18 +5,19 @@ ENV_FILE="environment.yml"
 
 # Function to initialize Conda in the script environment
 initialize_conda() {
-    __conda_setup="$('/Users/jshiver/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    __conda_setup="$("$HOME/miniconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
     if [ $? -eq 0 ]; then
         eval "$__conda_setup"
     else
-        if [ -f "/Users/jshiver/miniconda3/etc/profile.d/conda.sh" ]; then
-            . "/Users/jshiver/miniconda3/etc/profile.d/conda.sh"
+        if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+            . "$HOME/miniconda3/etc/profile.d/conda.sh"
         else
-            export PATH="/Users/jshiver/miniconda3/bin:$PATH"
+            export PATH="$HOME/miniconda3/bin:$PATH"
         fi
     fi
     unset __conda_setup
 }
+
 
 # Function to create a new Conda environment
 create_env() {
--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from mnist_loader import load_data_wrapper
 
 if __name__ == "__main__":
     training, validation, test = load_data_wrapper()
-    net = Network([784, 100, 10])
+    net = Network([784, 30, 10])
     net.SGD(training, 30, 10, 3.0, test_data=test)
